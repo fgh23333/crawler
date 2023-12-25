a1f39c9cb7f4c3b22c3e332ba99dca80392cde86
fgh23333 <635262140@qq.com>
2023-12-25 16:55:20 +0800

    change

--- rewrite.js
+++ rewrite.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-fs.readFile(__dirname + "/习概.json", (err, data) => {
+fs.readFile(__dirname + "/思政.json", (err, data) => {
     if (err) {
         console.error(err)
     } else {
@@ -17,14 +17,14 @@ fs.readFile(__dirname + "/习概.json", (err, data) => {
             if (data[i].StandardAnswer != '正确' && data[i].StandardAnswer != '错误') {
                 let optionsArray = data[i].OptionContent.split('|')
                 temp.option = optionsArray
-                temp.answer = data[i].StandardAnswer
+                temp.answer = data[i].StandardAnswer.split("")
             } else {
                 temp.option = ['正确', '错误']
                 temp.answer = data[i].StandardAnswer
             }
             content.push(temp)
         }
-        fs.writeFile(__dirname + "/习概_rewrite.json", JSON.stringify(content), (err) => {
+        fs.writeFile(__dirname + "/思政_rewrite.json", JSON.stringify(content), (err) => {
             if (err) {
                 console.error(err)
             } else {
