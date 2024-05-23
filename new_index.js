ff8ea0c28c27025bfe612c7dbb2a7a76ecb962ba
fgh23333 <635262140@qq.com>
2024-05-23 11:33:34 +0800

    Update new_index.js

--- new_index.js
+++ new_index.js
@@ -4,18 +4,21 @@ const baseUrl = 'http://222.73.57.153:6571';
 
 const targetPath = '/examinationInfo/getPracticeInfo';
 
-const data = {
+const params = {
     branchId: "1705139277953761280",
     chapterId: "",
     studentId: "1741125438396170311",
     subjectId: "1752935841845477392",
 }
 
-axios.post(baseUrl + targetPath, JSON.stringify(data), {
+axios.post(baseUrl + targetPath, JSON.stringify(params), {
     headers: {
         'Content-Type': 'application/json;charset=utf-8'
     }
 })
     .then(res => {
-        console.log(res.data);
+        let data = JSON.parse(res.data.data.paperStore.paperContent)
+        let answer = JSON.parse(res.data.paperStore.paperAnswer);
+        console.log(data);
+        let singleChoice = data.danxuan.children
     })
