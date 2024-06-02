537ac9b568e6a9b04f0adee6be18bff9901f7677
fgh23333 <635262140@qq.com>
2024-06-02 15:30:45 +0800

    Update new_index.js

--- new_index.js
+++ new_index.js
@@ -6,60 +6,60 @@ const baseUrl = 'http://222.73.57.153:6571';
 const targetPath = '/examinationInfo/getPracticeInfo';
 
 let paramsArr = [
-    // {
-    //     name: 'XiIntro',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1752935841845477392",
-    //     },
-    // },
-    // {
-    //     name: 'Marx',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1748167277460586496",
-    //     },
-    // },
-    // {
-    //     name: 'MaoIntro',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1748168736914800651",
-    //     },
-    // },
-    // {
-    //     name: 'Political',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1781216923707506688",
-    //     },
-    // },
-    // {
-    //     name: 'CMH',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1748168736914800640",
-    //     },
-    // },
-    // {
-    //     name: 'NCH',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1776854236110258176",
-    //     },
-    // },
+    {
+        name: 'XiIntro',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1752935841845477392",
+        },
+    },
+    {
+        name: 'Marx',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1748167277460586496",
+        },
+    },
+    {
+        name: 'MaoIntro',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1748168736914800651",
+        },
+    },
+    {
+        name: 'Political',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1781216923707506688",
+        },
+    },
+    {
+        name: 'CMH',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1748168736914800640",
+        },
+    },
+    {
+        name: 'NCH',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1776854236110258176",
+        },
+    },
     {
         name: 'SDH',
         params: {
@@ -69,15 +69,15 @@ let paramsArr = [
             subjectId: "1752935841845477376",
         },
     },
-    // {
-    //     name: 'ORH',
-    //     params: {
-    //         branchId: "1705139277953761280",
-    //         chapterId: "",
-    //         studentId: "1795023275139531171",
-    //         subjectId: "1752935841845477384",
-    //     },
-    // },
+    {
+        name: 'ORH',
+        params: {
+            branchId: "1705139277953761280",
+            chapterId: "",
+            studentId: "1795023275139531171",
+            subjectId: "1752935841845477384",
+        },
+    },
     {
         name: 'CCPH',
         params: {
